3454aed69eb8ff889216649a77ccf80c81bb8081
Dimitry Ushakov <dimitry.ushakov@rackspace.com>
2015-02-26 04:35:57 -0600

    update the test to make it stupid simple

--- roles/test/files/run_test.sh
+++ roles/test/files/run_test.sh
@@ -1,13 +1,1 @@
-virtualenv .
-. bin/activate
-pip install -rtest-requirements.txt
-pip install -rrequirements.txt
-pip install -U nose
-pip install pep8
-pip install -U coverage
-python app.py &amp;
-pid=$!
-nosetests functionaltests/ -v --all-modules --with-xunit --with-coverage --cover-xml
-kill -9 $pid
-
-deactivate
+curl $1
