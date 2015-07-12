d01b863f57e53321e4c3063ab3e4cfc7dbb827c8
dimtruck <dimitry.ushakov@rackspace.com>
2015-07-12 13:34:53 -0500

    moar tests

--- roles/test/files/run_test.sh
+++ roles/test/files/run_test.sh
@@ -1,1 +1,9 @@
-#curl $1
+
+
+PATH=/var/lib/jenkins/npm_global/bin:$PATH
+npm install
+npm install karma
+npm install karma-coverage
+npm install karma-jenkins-reporter
+bower install
+grunt test:e2e
